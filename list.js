36faca9cb184d873864be31c532fb78737a6f89d
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-03-17 12:38:58 -0400

    Update list.js

--- list.js
+++ list.js
@@ -65,7 +65,8 @@ class BinaryTree {
     if(node !== null){
       this._printTree(node.left)
       this._printTree(node.right)
-      document.writeln(node.data);
+      document.writeln(node.data + "");
+      
     }
   }
 
@@ -84,14 +85,92 @@ class BinaryTree {
   }
 
   roll(){
-    let n = 0;
-    for (let n = 0; n < 5; n++) {
+    for (let n = 0; n < 5; n++)
       this.insert(Math.floor(Math.random() * (7 - 1) ) + 1);
+  }
+
+  in_order_traversal()
+  {
+    this._in_order_traversal(this.root);
+  }
+  _in_order_traversal(node){
+    if(node !== null)
+    {
+      this._in_order_traversal(node.left);
+      document.writeln(node.data);
+      this._in_order_traversal(node.right);
     }
   }
- 
 
+  pre_order_traversal()
+  {
+    this._pre_order_traversal(this.root);
+  }
+
+  _pre_order_traversal(node){
+    if(node !== null)
+      {
+        document.writeln(node.data);
+        this._pre_order_traversal(node.left);
+        this._pre_order_traversal(node.right);
+      }
+  }
+
+  post_order_traversal(){
+    this._post_order_traversal(this.root)
+  }
+
+  _post_order_traversal(node){
+    if(node !== null)
+      {
+        this._pre_order_traversal(node.left);
+        this._pre_order_traversal(node.right);
+        document.writeln(node.data);
+      }
+  }
+
+
+  }
+
+
+//------------------------------------------------------------
+main();
+function main () {
+
+  let t = new BinaryTree();
+
+  /* const combinationsWithRepetition = getCombinationsWithRepetition([1,2,3,4,5,6], 5);
+      combinationsWithRepetition.forEach(element => {
+        let sum = 0;
+        element.forEach(item => {
+          sum += item;
+        })
+        t.insert(sum);
+  }); */
+
+  for (let index = 0; index < 8; index++)
+  {
+    t.roll();
+  }
+
+  aList = t.getListFromTree();
+  //t.printTree();
+  
+  document.write("<h1> in order traversal </h1>")
+  t.in_order_traversal();
+  
+  document.write("<h1>pre order traversal</h1> ")
+  t.pre_order_traversal();
+
+  document.write("<h1>post order traversal</h1> ")
+  t.post_order_traversal();
+
+  console.log(t);
 }
+
+
+
+
 
 //fibonacci(5)
 function fibonacci(n, memo = {})
@@ -128,39 +207,3 @@ function fibonacci(n, memo = {})
     return memo[n];
   }
 }
-
-
-main();
-
-function main () {
-
-  let t = new BinaryTree();
-
-  /* const combinationsWithRepetition = getCombinationsWithRepetition([1,2,3,4,5,6], 5);
-
-  combinationsWithRepetition.forEach(element => {
-    let sum = 0;
-    element.forEach(item => {
-      sum += item;
-    })
-
-    t.insert(sum);
-
-  }); */
-
-  for (let index = 0; index < 1; index++)
-  {
-    t.roll();
-    document.writeln("<br>");
-  }
-
-  aList = t.getListFromTree();
-
-  t.printTree();
-
-  console.log(t);
-  console.log(aList);
-
-}
-
-
