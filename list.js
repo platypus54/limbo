8c35611cc64baa78fe6dc3520d55be266c267a66
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-04-09 19:17:06 -0400

    Update list.js

--- list.js
+++ list.js
@@ -1,19 +1,3 @@
-let dice = new Array(5);
-let rolls = 0;
-
-q = [];
-
-
-//------------------------------------------------------------
-
-class Pair{
-  constructor(x, y){
-    this.item1 = x;
-    this.item2 = y;
-  }
-}
-
-
 class Node{
 
   constructor(data){
@@ -28,201 +12,292 @@ class Node{
       isParent: false,
       isleaf: false
     }
-  }
 
+  }
 }
 
 class BinaryTree {
 
-  constructor(){
-      this.root = null;
-  }
+find_max()
+{
+  return this.#_find_max(this.root);
+}
 
-  find_min(){
-    return this._find_min(this.root);
-  }
 
-  _find_min(node)
-  {
-    if(node !== null)
-    {
-        this._find_min(node.left)
-        if(node > node.left)
-          console.log("hello?")
-    }
-  }
+find_min()
+{
+  return this.#_find_min(this.root);
+}
 
-  
-  isEmpty(){
-    if(this.root == null)
-      return true;
+buildEmptyTreeFromList(init_list)
+{
+    if(this.root === null)
+        for (let index = 0; index < init_list.length; index++) 
+          this.insert(init_list[index]);
     else
-      return false;
+      console.log("tree is not empty")
+}
+
+isEmpty(){
+  if(this.root === null)
+    return true;
+  else
+    return false;
+}
+
+remove(data){
+  this.root = this.#_remove(this.root, data)
+}
+
+insert(data)
+{
+  this.root = this.#_insert(this.root,data);
+} 
+
+ getListFromTree()
+ {
+  let hereIsAListToBeMadeFromATree = [];
+  this.#_getListFromTree(this.root,hereIsAListToBeMadeFromATree)
+  return hereIsAListToBeMadeFromATree;
+}
+
+printTree()
+{
+  this.#_printTree(this.root);
+}
+
+roll()
+{
+  for (let n = 0; n < 10; n++)
+    this.insert(Math.floor(Math.random() * (10 - 1) ) + 1);
+}
+
+in_order_traversal()
+{
+  this.#_in_order_traversal(this.root, 0);
+}
+
+pre_order_traversal()
+{
+  this.#_pre_order_traversal(this.root, 0);
+}
+
+post_order_traversal()
+{
+  this.#_post_order_traversal(this.root, 0)
+}
+
+get_depth()
+{
+  this.#_get_depth(this.root,0);
+}
+
+get_height()
+{
+  this.#_get_height(this.root,0);
+}
+
+find_leaves()
+{
+  this.#_find_leaves(this.root);
+}
+
+find_parents()
+{
+  this.#_find_parents(this.root);
+}
+
+colorNode(node){
+      if(node == this.root)
+        document.writeln(`<p style="color:blue"> ${node.data}</p>`); // is root
+      else if(node.left == null && node.right == null)
+        document.writeln(`<p style="color:green"> ${node.data}</p>`); // is leaf
+      else if(node.left != null || node.right != null)
+        document.writeln(`<p style="color:orange"> ${node.data}</p>`); // is parent
+}
+
+colorNode2(node, depth){
+  if(node == this.root)
+    document.writeln(`(${depth},${node.data})`); // is root
+  else if(node.left == null && node.right == null)
+    document.writeln(`(${depth},${node.data})`); // is leaf
+  else if(node.left != null || node.right != null)
+    document.writeln(`(${depth},${node.data})`); // is parent
+}
+
+/***********************************/
+
+  constructor()
+  {
+    this.root = null;
   }
 
-  insert(data){
-     this.root = this._insert(this.root,data);
-  } 
+  #_find_max(node){
+    
+    if(node === null)
+      return null;
+    else if(node.right === null)
+     return node.data
+    else
+      return this.#_find_max(node.right)
+    
+    }
 
-  _insert(node, value) {
+  #_find_min(node)
+  {
+    if(node === null)
+      return null;
+    else if(node.left === null)
+     return node;
+    else
+      return this.#_find_min(node.left)
+    
+  }
+
+
+  #_remove(node, data){
+    if(node === null)
+      return null;
+    
+    if(data < node.data)
+    {
+      node.left = this.#_remove(node.left, data);
+    }
+    else if(data > node.data)
+    {
+      node.right = this.#_remove(node.right,data);
+    }
+    else if(node.left !== null && node.right !== null)
+    {
+      let min_node = this.#_find_min(node.right);
+      node.data = min_node.data;
+      node.right = this.#_remove(node.right, node.data,);
+    }
+    else
+    {
+       if(node.left !== null)
+          node = node.left;
+        else 
+          node = node.right; 
+    }
+    return node;
+
+    
+  }
+ 
+
+  #_insert(node, value) {
 
     if(node === null)
        return new Node(value);
-    else if(value <= node.data)
-    {
-      node.left = this._insert(node.left, value);
-    }
+    else if(value < node.data)
+      node.left = this.#_insert(node.left, value);
     else if (value > node.data)
-    {
-      node.right = this._insert(node.right,value);
-    }
+      node.right = this.#_insert(node.right,value);
+
     return node;
   }
 
-  printTree(){
-    this._printTree(this.root);
-  }
 
-  _printTree(node){
+  #_printTree(node){
     if(node !== null){
-      this._printTree(node.left)
-      this._printTree(node.right)
+      this.#_printTree(node.left)
+      this.#_printTree(node.right)
       document.writeln(node.data + "");
     }
   }
 
-  getListFromTree(){
-    let hereIsAListToBeMadeFromATree = [];
-    this._getListFromTree(this.root,hereIsAListToBeMadeFromATree)
-    return hereIsAListToBeMadeFromATree;
-  }
 
-  _getListFromTree(node, list){
+
+  #_getListFromTree(node, list){
     if(node !== null){
 
       let p = new Pair(node._meta_data.depth, node.data)
       list.push(p);
-      this._getListFromTree(node.left,list);
-      this._getListFromTree(node.right,list);
+      this.#_getListFromTree(node.left,list);
+      this.#_getListFromTree(node.right,list);
       
     }
   }
 
-  roll(){
-    for (let n = 0; n < 10; n++)
-      this.insert(Math.floor(Math.random() * (10 - 1) ) + 1);
-  }
-
-  in_order_traversal()
-  {
-    this._in_order_traversal(this.root, 0);
-  }
+  
  
-  _in_order_traversal(node, depth){
+  #_in_order_traversal(node, depth){
     if(node !== null)
     {
       
-      this._in_order_traversal(node.left, depth + 1);
+      this.#_in_order_traversal(node.left, depth + 1);
       this.colorNode2(node, depth);
-      this._in_order_traversal(node.right, depth + 1);
+      this.#_in_order_traversal(node.right, depth + 1);
 
     }
   }
 
-  pre_order_traversal()
-  {
-    this._pre_order_traversal(this.root, 0);
-  }
+  
 
   // {root, left tree, right tree}
-  _pre_order_traversal(node, depth){
+  #_pre_order_traversal(node, depth){
     if(node !== null)
       {
-        //this.colorNode2(node,depth);
-        this._pre_order_traversal(node.left, depth + 1);
-        this._pre_order_traversal(node.right, depth + 1);
+        this.colorNode2(node,depth);
+        this.#_pre_order_traversal(node.left, depth + 1);
+        this.#_pre_order_traversal(node.right, depth + 1);
       }
   }
 
   // {root, right tree, left tree}
-  post_order_traversal(){
-    this._post_order_traversal(this.root, 0)
-  }
+ 
 
-  _post_order_traversal(node, depth){
+  #_post_order_traversal(node, depth){
     if(node !== null)
       {     
-        this._post_order_traversal(node.left, depth + 1);
-        this._post_order_traversal(node.right, depth + 1);
+        this.#_post_order_traversal(node.left, depth + 1);
+        this.#_post_order_traversal(node.right, depth + 1);
         this.colorNode2(node, depth);
       }
   }
 
-  get_depth(){
-    this._get_depth(this.root,0);
-  }
   
-  _get_depth(node,depth){
+  #_get_depth(node,depth){
     
       if(node !== null)
       {
           node._meta_data.depth = depth;
           this.colorNode2(node,depth);
-          this._get_depth(node.left,depth + 1);
-          this._get_depth(node.right,depth + 1);
+          this.#_get_depth(node.left,depth + 1);
+          this.#_get_depth(node.right,depth + 1);
       }
   }
 
-  get_height(){
-    this._get_height(this.root,0);
-  }
-  _get_height(){}
+  
+
+  #_get_height(){}
 
 
-  find_leaves(){
-    this._find_leaves(this.root);
-  }
-  _find_leaves(node){
+  #_find_leaves(node){
       if( node !== null)
       {
         if(node.left == null && node.right == null)
           node._meta_data.isleaf = true;
-          this._find_leaves(node.left);
-          this._find_leaves(node.right);
+          this.#_find_leaves(node.left);
+          this.#_find_leaves(node.right);
       } 
   }
 
-  find_parents(){
-    this._find_parents(this.root);
-  }
 
-  _find_parents(node){
+  
+
+  #_find_parents(node){
     if( node !== null)
       {
           if(node.left != null || node.right != null)
             node._meta_data.isParent = true;
-          this._find_parents(node.left);
-          this._find_parents(node.right);
+          this.#_find_parents(node.left);
+          this.#_find_parents(node.right);
       } 
   }
 
-  colorNode(node){
-        if(node == this.root)
-          document.writeln(`<p style="color:blue"> ${node.data}</p>`); // is root
-        else if(node.left == null && node.right == null)
-          document.writeln(`<p style="color:green"> ${node.data}</p>`); // is leaf
-        else if(node.left != null || node.right != null)
-          document.writeln(`<p style="color:orange"> ${node.data}</p>`); // is parent
-  }
-
-  colorNode2(node, depth){
-    if(node == this.root)
-      document.writeln(`(${depth},${node.data})`); // is root
-    else if(node.left == null && node.right == null)
-      document.writeln(`(${depth},${node.data})`); // is leaf
-    else if(node.left != null || node.right != null)
-      document.writeln(`(${depth},${node.data})`); // is parent
-}
+  
 }
 
 function getCombinationsWithRepetition(arr, length, start = 0, current = [], result = []) {
