01d2bda991c6b0ec4822ab84fa5881ad28103780
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-04-08 22:24:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,27 +5,25 @@ function main () {
   let t = new BinaryTree();
   let emptyTree = new BinaryTree();
 
-  console.log(emptyTree.isEmpty());
+  root_node = new Node(100);
+  node1 = new Node(25);
+  node2 = new Node(101);
 
-  /* const combinationsWithRepetition = getCombinationsWithRepetition([1,2,3,4,5,6], 5);
-      combinationsWithRepetition.forEach(element => {
-        let sum = 0;
-        element.forEach(item => {
-          sum += item;
-        })
-        t.insert(sum);
-  }); */
-
-  for (let index = 0; index < 1; index++)
-  {
-    t.roll();
-  }
-
-  node1 = new Node();
-  node1.data = 100;
-  insertIntoTree(node1,t.root)
+  insert(root_node,node1);
+  insert(root_node,node2);
+  
+  console.log()
 
   
+
+  for (let index = 0; index < 1; index++)
+    t.roll();
+
+
+  insert(t.root,root_node)
+
+
+  // display results....
   document.writeln("<h1> in order traversal </h1>")
   t.in_order_traversal();
   
@@ -38,48 +36,31 @@ function main () {
   document.writeln("<h1>depth</h1> ")
   t.get_depth();
 
-  t.find_leaves();
-  t.find_parents();
-
-  emptyTree.insert("NOT_EMPTY");
-
-  emptyTree.find_leaves();
-  emptyTree.find_parents();
-
-  aList = t.getListFromTree();
-
-  console.log(aList)
+  
+  
+  
   console.log(t);
-  console.log(emptyTree);
 
 }
 
-function insertIntoTree(node_to_insert,tree_node)
+// we connect nodes together by searching the tree and finding where the
+// data should fall depending on criteria. Here we have two nodes: tree_node 
+// and node_to_insert.
+function insert(tree_node, node_to_insert)
 {
     if(tree_node === null)
-      return node_to_insert;
-    else if(tree_node.data  <= node_to_insert.data)
-      tree_node.left = insertIntoTree(node_to_insert, tree_node.left) ;
-    else if(tree_node.data  >= node_to_insert.data)
-      tree_node.right = insertIntoTree(node_to_insert, tree_node.right);
-    return node_to_insert;
-  
+        return new Node(node_to_insert.data);
+    else if(tree_node.data < node_to_insert.data )
+      tree_node.left = insert(tree_node.left, node_to_insert);
+    else if(tree_node.data > node_to_insert.data )
+      tree_node.right = insert(tree_node.right, node_to_insert);
+
+    return tree_node;
 }
 
-function checkTypeLess(node_to_insert, tree_node)
-{
-    if( typeof(node_to_insert.data) == "String" && typeof(tree_node.data) == "String" ) {
-      return (node_to_insert.data).length < (tree_node.data).length 
-    }
-    else
-      return tree_node.data  < node_to_insert.data;
-}
 
-function checkTypeGreater(node_to_insert, tree_node)
-{
-  if( typeof(node_to_insert.data) == "String" && typeof(tree_node.data) == "String" ) {
-    return (node_to_insert.data).length > (tree_node.data).length 
-  }
-  else
-    return tree_node.data  > node_to_insert.data;
-}
+
+/* if(tree_node.data  < node_to_insert.data)
+  tree_node.left = insertIntoTree(tree_node.left, node_to_insert);
+else if(tree_node.data  > node_to_insert.data)
+  tree_node.right = insertIntoTree(tree_node.right, node_to_insert); */
